8ac378466d3a1bb85d13d10bc6c8fbd84a001474
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2023-02-11 22:54:52 -0300

    mongodb connection, server, routes

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const userRoute = require('./routes/user'); 
+const authRoute = require('./routes/auth'); 
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute)
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log('DB connection successfull!'))
+    .catch((err) => console.log(err));
+
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log('backend server is running');
+})
