104f0364b88039c24d44df4e18693c0b16aba9d3
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2023-02-11 23:51:05 -0300

    users update

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,24 @@
+const bcrypt = require('bcryptjs');
 const router = require('express').Router();
 
+const { verifyToken, verifyTokenAndAuthorization } = require('../middleware/verifyToken');
+const User = require('../models/User');
 
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+  try{
+    if(req.body.password){
+      req.body.password = await bcrypt.hash(req.body.password, 8);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+      $set: req.body
+    }, { new: true });
+
+    return res.status(200).json(updatedUser);
+  }catch(err){
+    return res.status(500).json(err);
+  }
+
+})
 
 module.exports = router;
