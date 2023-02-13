f578e39a48b0538fe9f5205fb78af9ef56f9b07a
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2023-02-12 22:28:05 -0300

    slider

---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/yXL5yjg/pngkey-com-fashion-model-png-882197.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/wyDwph9/27727-4-fashion-model-image.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/PGCtbGc/purepng-com-fashion-girlwomenpeoplepersonsfemalefashion-11215251135953rklf.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
