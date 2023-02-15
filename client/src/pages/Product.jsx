4049041a14082394299c758cc5e7e6fdc4af2286
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2023-02-14 22:25:08 -0300

    single product page

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -3,9 +3,100 @@ import Announcement from "../components/Announcement"
 import Navbar from "../components/Navbar"
 import Newsletter from "../components/Newsletter"
 import Footer from "../components/Footer"
+import { Add, Remove } from "@material-ui/icons"
 
 
-const Container = styled.div``
+const Container = styled.div`
+
+`;
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+const InfoContainer = styled.div`
+  margin-top: 20px;
+  flex: 1;
+  padding: 0 50px;
+`;
+const Title = styled.h1`
+  font-weight: 200;
+`;
+const Desc = styled.p`
+  margin: 20px 0;
+`;
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0;
+  display: flex;
+  justify-content: space-between;
+`;
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${ props => props.color };
+  margin: 0 5px;
+  cursor: pointer;
+`;
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+  background-color: #fff;
+  border: 1px solid gray;
+`;
+const AddContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 50%;
+  justify-content: space-between;
+  `;
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 5px;
+  `;
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: #fff;
+  cursor: pointer;
+  font-weight: 500;
+  
+  &:hover{
+    background-color: #f8f4f4;
+  }
+`;
+const FilterSizeOption = styled.option``;
 
 
 const Product = () => {
@@ -13,6 +104,44 @@ const Product = () => {
     <Container>
       <Navbar />
       <Announcement />
+      
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Desc>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit saepe harum nesciunt eos, eum voluptas nam distinctio amet soluta veniam repudiandae, dolorem, et aspernatur doloremque? Alias illo perspiciatis facilis quo?</Desc>
+          <Price>$ 20</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>ADD TO CARD</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      
       <Newsletter />
       <Footer />
     </Container>
