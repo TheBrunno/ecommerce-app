0020b0b44e8a23de80bf451ed92f782d28710245
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2023-02-15 22:23:33 -0300

    newsletter responsivity

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -1,6 +1,6 @@
-import { Send } from '@material-ui/icons'
-import React from 'react'
-import styled from 'styled-components'
+import { Send } from '@material-ui/icons';
+import styled from 'styled-components';
+import { mobile } from '../responsive';
 
 const Container = styled.div`
   height: 60vh;
@@ -9,6 +9,7 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  ${ mobile({ height: "45vh" }) }
 `;
 const Title = styled.h1`
   font-size: 70px;
@@ -18,6 +19,7 @@ const Desc = styled.div`
   font-size: 24px;
   font-weight: 300;
   margin-bottom: 20px;
+  ${ mobile({ textAlign: "center" }) }
 `;
 const InputContainer = styled.div`
   width: 50%;
@@ -26,6 +28,7 @@ const InputContainer = styled.div`
   display: flex;
   justify-content: space-between;
   border: 1px solid lightgray;
+  ${ mobile({ width: "80%" }) }
 `;
 const Input = styled.input`
   border: none;
