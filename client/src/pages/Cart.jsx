9f0302062cda867c0f584990c9ea7c9ac21ac55f
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2023-02-15 21:38:20 -0300

    size correction

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -48,7 +48,7 @@ const Summary = styled.div`
   border: 0.5px solid lightgray;
   border-radius: 10px;
   padding: 20px;
-  height: 50vh;
+  height: max-content;
 `;
 const Product = styled.div`
   display: flex;
