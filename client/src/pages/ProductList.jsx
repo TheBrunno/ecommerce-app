58373710b22aa1a0b998c7883a49da52b3637f62
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2023-02-15 22:40:45 -0300

    product list page responsivity

--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -1,9 +1,10 @@
-import styled from "styled-components"
-import NavBar from "../components/Navbar"
-import Announcement from "../components/Announcement"
-import Products from "../components/Products"
-import Newsletter from "../components/Newsletter"
-import Footer from "../components/Footer"
+import styled from "styled-components";
+import NavBar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+import { mobile } from '../responsive';
 
 
 const Container = styled.div``;
@@ -16,17 +17,21 @@ const FilterContainer = styled.div`
 `;
 const Filter = styled.div`
   margin: 20px;
+  ${ mobile({ width: "0 20px", display: "flex", flexDirection: "column" }) }
 `;
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
+  ${ mobile({ marginRight: "0" }) }
+
 `;
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
   background-color: #fff;
   border: 1px solid gray;
+  ${ mobile({ margin: "10px 0" }) }
 `;
 const Option = styled.option`
   
