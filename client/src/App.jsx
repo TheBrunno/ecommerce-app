d8e0ba5d2f8b70ca37042cb15b04fe56817902d9
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2023-02-15 22:45:52 -0300

    single product page responsivity

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 // import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-// import Product from "./pages/Product";
+// import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 // import Register from "./pages/Register";
 // import Login from "./pages/Login";
 // import Cart from "./pages/Cart";
 
 const App = () => {
-  return <ProductList/>;
+  return <Product/>;
 };
 
 export default App;
--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,9 +1,10 @@
-import styled from "styled-components"
-import Announcement from "../components/Announcement"
-import Navbar from "../components/Navbar"
-import Newsletter from "../components/Newsletter"
-import Footer from "../components/Footer"
-import { Add, Remove } from "@material-ui/icons"
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+import { Add, Remove } from "@material-ui/icons";
+import { mobile } from "../responsive";
 
 
 const Container = styled.div`
@@ -12,6 +13,8 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 50px;
   display: flex;
+  ${ mobile({ padding: "10px", flexDirection: "column" }) }
+
 `;
 const ImgContainer = styled.div`
   flex: 1;
@@ -20,11 +23,13 @@ const Image = styled.img`
   width: 100%;
   height: 90vh;
   object-fit: cover;
+  ${ mobile({ height: "60vh" }) }
 `;
 const InfoContainer = styled.div`
   margin-top: 20px;
   flex: 1;
   padding: 0 50px;
+  ${ mobile({ padding: "10px" }) }
 `;
 const Title = styled.h1`
   font-weight: 200;
@@ -41,6 +46,8 @@ const FilterContainer = styled.div`
   margin: 30px 0;
   display: flex;
   justify-content: space-between;
+  ${ mobile({ width: "100%" }) }
+
 `;
 const Filter = styled.div`
   display: flex;
@@ -69,7 +76,8 @@ const AddContainer = styled.div`
   align-items: center;
   width: 50%;
   justify-content: space-between;
-  `;
+  ${ mobile({ width: "100%" }) }
+`;
 const AmountContainer = styled.div`
   display: flex;
   align-items: center;
