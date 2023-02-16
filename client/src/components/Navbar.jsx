e0438814089c00c8f205c71ea1e1afbd8069e0c0
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2023-02-15 22:12:52 -0300

    navbar responsive

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,21 @@
 import { Badge } from '@material-ui/core';
 import { Search, ShoppingCartOutlined } from '@material-ui/icons';
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import { mobile } from '../responsive';
 
 const Container = styled.div`
   height: 60px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, .2);
-`
+  ${ mobile({ height: "50px" }) }
+`;
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-`
+  ${ mobile({ padding: "10px 0" }) }
+`;
 
 const Left = styled.div`
   flex: 1;
@@ -23,6 +26,7 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${ mobile({ display: "none" }) }
 `;
 
 const SerchContainer = styled.div`
@@ -35,6 +39,7 @@ const SerchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${ mobile({ width: "50px" }) }
 `;
 
 const Center = styled.div`
@@ -44,6 +49,7 @@ const Center = styled.div`
 
 const Logo = styled.h1`
   font-weight: bold;
+  ${ mobile({ fontSize: "24px" }) }
 `;
 
 const Right = styled.div`
@@ -51,12 +57,14 @@ const Right = styled.div`
   align-items: center;
   justify-content: flex-end;
   flex: 1;
+  ${ mobile({ justifyContent: "center", flex: 2 }) }
 `;
 
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${ mobile({ fontSize : "12px", marginLeft: '10px' }) }
 `;
 
 const Navbar = () => {
@@ -66,7 +74,7 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SerchContainer>
-            <Input />
+            <Input placeholder='Search'/>
             <Search style={{color: 'gray', fontSize: 16}}/>
           </SerchContainer>
         </Left>
