d91c3164d977e6791b7ac9785bc3152e88d8e472
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2023-02-15 22:20:38 -0300

    responsivity categories

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import { categories } from '../data';
 import CategoriyItem from './CategoryItem';
+import { mobile } from '../responsive';
 
 const Container = styled.div`
   display: flex;
   padding: 20px;
   justify-content: space-between;
+  ${ mobile({ padding: "0", flexDirection: "column" }) }
 `;
 
 const Categories = () => {
