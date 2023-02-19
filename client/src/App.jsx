e558c186829a9717e35665ba03678848b1b9b65e
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2023-02-19 12:18:07 -0300

    react-router-dom add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,27 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from 'react-router-dom';
+
 const App = () => {
-  return <Home/>;
+  const user = true;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/cart" element={<Cart/>}/>
+        <Route exact path="/login" element={user ? <Navigate to="/"/> :<Login/>}/>
+        <Route exact path="/products/:category" element={<ProductList/>}/>
+        <Route exact path="/product/:id" element={<Product/>}/>
+        <Route exact path="/register" element={user ? <Navigate to="/"/> :<Register/>}/>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
